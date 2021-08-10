d6c3ea7c02e7e98852b0ba925c076c1973652803
Bernard van Gastel <bvgastel@bitpowder.com>
2021-08-10 10:26:32 +0200

    Initial WSL support.

--- lib.cpp
+++ lib.cpp
@@ -11,24 +11,26 @@
 #include <pwd.h>
 
 #include <stdexcept>
+#include <fstream>
+#include <filesystem>
 
 bool IsFile(std::string file) {
-	struct stat st;
-	if (stat(file.c_str(), &st) != 0) {
-		if (errno == ENOENT)
-			return false;
-		throw std::invalid_argument(std::string("File::IsFile: could not stat() file ") + file);
-	}
-	return S_ISREG(st.st_mode) || S_ISLNK(st.st_mode);
+  struct stat st;
+  if (stat(file.c_str(), &st) != 0) {
+    if (errno == ENOENT)
+      return false;
+    throw std::invalid_argument(std::string("File::IsFile: could not stat() file ") + file);
+  }
+  return S_ISREG(st.st_mode) || S_ISLNK(st.st_mode);
 }
 bool IsSocket(std::string file) {
-	struct stat st;
-	if (stat(file.c_str(), &st) != 0) {
-		if (errno == ENOENT)
-			return false;
-		throw std::invalid_argument(std::string("File::IsFile: could not stat() file ") + file);
-	}
-	return S_ISSOCK(st.st_mode);
+  struct stat st;
+  if (stat(file.c_str(), &st) != 0) {
+    if (errno == ENOENT)
+      return false;
+    throw std::invalid_argument(std::string("File::IsFile: could not stat() file ") + file);
+  }
+  return S_ISSOCK(st.st_mode);
 }
 std::string Contents(int fd, bool& eof, size_t max) {
   std::string retval;
@@ -101,11 +103,51 @@ std::string GetUsername() {
   return {};
 }
 
+static std::ifstream OpenFile(std::string filename) {
+  auto p = std::filesystem::path(filename.begin(), filename.end());
+  std::ifstream infile;
+  infile.open(p, std::ios::binary);
+  if (!infile.is_open())
+    throw std::invalid_argument("could not open file: " + filename);
+
+  // pos == max long on Linux if reading a directory
+  if (!infile.good())
+    throw std::invalid_argument("could not get size of file (possibly a dir): " + filename);
+  return infile;
+}
+
+std::string FileContents(std::string filename) {
+  auto infile = OpenFile(filename);
+
+  std::string retval;
+  char buffer[16384];
+
+  do {
+    infile.read(buffer, sizeof(buffer));
+    retval.append(buffer, 0, infile.gcount());
+  } while (infile.good());
+
+  if (infile.bad())
+    throw std::invalid_argument("could not read file: " + filename);
+
+  infile.close();
+  return retval;
+}
+
+bool IsOnWSL() {
+  std::string version = FileContents("/proc/version");
+  return version.find("icrosoft") != std::string::npos;
+}
+
 std::string GetClipboard(std::vector<int> closeAfterFork) {
 #if defined(__APPLE__)
   std::vector<std::string> getClipboardCommand = {"pbpaste"};
 #else
-  std::vector<std::string> getClipboardCommand = {"xsel", "--clipboard", "--output"}
+  std::vector<std::string> getClipboardCommand = {"xsel", "--clipboard", "--output"};
+  bool wsl = IsOnWSL();
+  if (wsl) {
+    getClipboardCommand = {"powershell.exe", "Get-Clipboard"};
+  }
 #endif
   auto [rfd, wfd, pid] = ExecRedirected(getClipboardCommand, false, closeAfterFork);
   close(wfd);
@@ -118,6 +160,10 @@ std::string GetClipboard(std::vector<int> closeAfterFork) {
     retval = retval + std::string(buffer, bytes);
   }
   close(rfd);
+  if (wsl) {
+    // `powershell.exe Get-Clipboard` appends \r\n to output, get rid of it
+    retval = retval.substr(0, retval.size()-2);
+  }
   return retval;
 }
 
@@ -125,7 +171,10 @@ bool SetClipboard(std::string clipboard, std::vector<int> closeAfterFork) {
 #if defined(__APPLE__)
   std::vector<std::string> setClipboardCommand = {"pbcopy"};
 #else
-  std::vector<std::string> setClipboardCommand = {"xsel", "--clipboard", "--input"}
+  std::vector<std::string> setClipboardCommand = {"xsel", "--clipboard", "--input"};
+  if (IsOnWSL()) {
+    setClipboardCommand = {"clip.exe"};
+  }
 #endif
   auto [rfd, wfd, pid] = ExecRedirected(setClipboardCommand, false, closeAfterFork);
   close(rfd);
