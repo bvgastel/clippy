272d8fca08280f285375169d326ae0f9b68c8d39
Bernard van Gastel <bvgastel@bitpowder.com>
2021-08-15 14:59:01 +0200

    Proper parsing of tmux variables.

--- remote.cpp
+++ remote.cpp
@@ -37,7 +37,10 @@ std::optional<std::string> RemoteSession() {
   std::string socket_path;
   if (getenv("TMUX")) {
     auto clippy = GetTMUXVariable("LC_CLIPPY", {});
-    socket_path = clippy ? *clippy : "";
+    if (clippy) {
+      auto pos = clippy->find("=");
+      socket_path = pos != clippy->npos ? clippy->substr(pos+1) : "";
+    }
   } else {
     auto clippy = getenv("LC_CLIPPY");
     socket_path = clippy ? clippy : "";
